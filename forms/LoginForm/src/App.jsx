141bcc031acba45beef319ebd5803a20f3bedf0b
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-05-10 16:30:34 +0100

    Login form project with controlled components

---
+++ forms/LoginForm/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1>Login Form</h1>
+     
+      <p className="read-the-docs">
+        Using Controlled Components 
+      </p>
+      <p>Jonatas Ribeiro</p>
+    </>
+  )
+}
+
+export default App

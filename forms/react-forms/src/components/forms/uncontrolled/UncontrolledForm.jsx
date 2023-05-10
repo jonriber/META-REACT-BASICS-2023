fd24c3b8b0fb36da34fdffc27a3909f40de9f3d4
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-05-10 15:42:54 +0100

    changes to Uncontrolled component

--- forms/react-forms/src/components/forms/uncontrolled/UncontrolledForm.jsx
+++ forms/react-forms/src/components/forms/uncontrolled/UncontrolledForm.jsx
@@ -4,7 +4,8 @@ import { useRef } from "react";
 const UncontrolledForm = () => {
     const inputRef = useRef(null);
 
-    function handleSubmit(){
+    function handleSubmit(e){
+        e.preventDefault();
         console.log("handle Submit Function")
         const inputValue = inputRef.current.value
         console.log("inputValue:",inputValue);
@@ -14,7 +15,12 @@ const UncontrolledForm = () => {
             <h3>Uncontrolled Component</h3>
             <p>Using input value stored in REAL DOM with useREF</p>
             <form onSubmit={handleSubmit}>
-                <input ref={inputRef} type="text"/>
+                <fieldset style={{display:"flex",flexDirection:"column", justifyContent:"center", alignItems:"normal"}}>
+                    <label>Input Text here:</label>
+                    <input ref={inputRef} type="text"/>
+
+                </fieldset>
+                <button type="submit">Submit</button>
             </form>
         </>
     )

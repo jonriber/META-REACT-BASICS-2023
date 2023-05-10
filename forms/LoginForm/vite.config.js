a11a2a44ed091eaadbd4c86c253536ccce3c6893
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-05-10 19:01:02 +0100

    changing repository url deployment

--- forms/LoginForm/vite.config.js
+++ forms/LoginForm/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/META-REACT-BASICS-2023",
+  base:"/META-REACT-BASICS-2023/forms/LoginForm",
   plugins: [react()],
 })

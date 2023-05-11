168bbedc4be2669636b5606e04adb74ad1820b3e
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-05-11 15:55:47 +0100

    updating style and text of login form

--- forms/LoginForm/src/App.jsx
+++ forms/LoginForm/src/App.jsx
@@ -6,13 +6,26 @@ function App() {
 
   return (
     <>
+      <div className='container'>
+        <h1>Login Form</h1>
+        <Login />
+        <h3>Development:</h3>
+        <p className="read-the-docs">
+          Using React Controlled Components with a local state.
+        </p>
+        <p className="read-the-docs">
+          This way, I´m enabled to use real-time input validation (password and email).
+        </p>
+        <p className="read-the-docs">
+          Made with <b>Vite</b> bundler and deployment with <b>github actions</b>.
+        </p>
+        <div>
+          <h3>Check the code at my github repo <a href='https://github.com/jonriber/META-REACT-BASICS-2023/tree/main/forms/LoginForm'>HERE</a></h3>
+        </div>
+
+      </div>
       
-      <h1>Login Form</h1>
-      <Login />
-      <p className="read-the-docs">
-        Using Controlled Components 
-      </p>
-      <p>Jonatas Ribeiro</p>
+      <footer><b>Jonatas Ribeiro</b>. 2023#</footer>
     </>
   )
 }

b18d45cd878186e824771bd5f6f72c5916c65e43
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-05-17 14:52:08 +0100

    Testing some component composition with my mentor

--- componentComposition_customHooks/componentComposition/src/components/RadioGroup.jsx
+++ componentComposition_customHooks/componentComposition/src/components/RadioGroup.jsx
@@ -1,9 +1,13 @@
 /* eslint-disable react/prop-types */
 import { Children, cloneElement } from "react"
 
+//run time, dinâmico
 const RadioGroup = ({onChange,selected,children}) => {
+
     const radioOptions = Children.map(children,(child) =>{
-        return cloneElement(child,{onChange,checked:child.props.value === selected})
+        return cloneElement(child,{
+            onChange,checked:child.props.value === selected
+        })
     })
 
     return (
@@ -11,7 +15,45 @@ const RadioGroup = ({onChange,selected,children}) => {
             {radioOptions}
         </>
     )
-
 }
 
 export default RadioGroup
+
+// const Teste = ({children}) => {
+
+//     return(
+//         <>
+//             {children}
+//         </>
+//     )
+// }
+
+// const buttonProps ={
+//     text:"texto padrão",
+//     style:{
+//         color:"primary",
+//         backgoundColor:"black"
+//     }
+// }
+
+// //jeito normal
+// const CustomButton = ({children,color,new}) => {
+//     const customStyle ={
+//         color:color,
+//     }
+//     if(variant==="a"){
+//         style
+//     }
+//     return(
+//     <>
+//         <button style={customStyle}></button>
+//     </>
+//     )
+// }
+
+// <div>
+//     <Teste>
+//         coisas aqui dentro
+//         <CustomButton new={"coisa nova"} color="azul" variant={} {...props}/>
+//     </Teste>
+// </div>
